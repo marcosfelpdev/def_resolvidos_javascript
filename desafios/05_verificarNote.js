b607dc8c4a710f5e0664d79aa36a730b35e6a6ac
Marcos <msa.felipesousa@gmail.com>
2025-10-14 10:50:46 -0300

    Correções

--- desafios/05_verificarNote.js
+++ desafios/05_verificarNote.js
@@ -6,14 +6,15 @@ let nota1 = Number(prompt(`Informe sua primeira nota: `))
 let nota2 = Number(prompt(`Informe sua primeira nota: `))
 let nota3 = Number(prompt(`Informe sua primeira nota: `))
 
-const lista = [nota1, nota2, nota3]
+const lista = []
+lista.push(nota1, nota2, nota3)
 let soma = 0
-function calcular(lista){
-    for(i = 0;i < lista.length; i++){
+function calcular(listaDeNotas){
+    for(i = 0;i < listaDeNotas.length; i++){
         soma += lista[i]
     }
     
-    let valorFinal = soma/3
+    let valorFinal = soma/listaDeNotas.length
 
     if(valorFinal >= 7){
         console.log(`Você está aprovado`)
