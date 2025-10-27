9c6a78f644b960ded342df59502e989c75b32a3a
Marcos <msa.felipesousa@gmail.com>
2025-10-26 23:48:21 -0300

    Ajustes e aprimoramento do código do exercício: 04_operacoes.js

--- desafios/04_operacoes.js
+++ desafios/04_operacoes.js
@@ -1,37 +1,50 @@
 const prompt = require("prompt-sync")();
 
-const numero01 = Number(prompt('Digite o primeiro número: '))
-const numero02 = Number(prompt('Digite o segundo número: '))
-const operacao = Number(prompt(`
+
+ini();
+
+function ini(){
+    const operacao = Number(prompt(`
     Digite o número correspondente a operação que deseja executar:
     1 - Soma
     2 - Subtração
     3 - Multiplicação
     4 - Divisão:
-`))
-
-function calcular (n1, n2, operacao){
     
-    if(operacao === 1){
-        soma = (n1+n2)
-        console.log(`Você escolheu 1 - soma, a operação entre os números ${n1} e ${n2} é igual a: ${soma}`)
-        return
-    }else if(operacao === 2){
-        subtracao = (n1-n2)
-        console.log(`você escolheu 2 - Subtração, a operação entre os números ${n1} e ${n2} é igual a: ${subtracao}`)
-        return
-    }else if(operacao === 3){
-        multiplicacao = (n1*n2)
-        console.log(`Você escolheu 3 - multiplicação, a operação entre os números ${n1} e ${n2} é igual a: ${multiplicacao}`)
-        return
-    }else if(operacao === 4){
-        divisao = (n1/n2)
-        console.log(`Você escolheu 4 - divisão, a operação entre os números ${n1} e ${n2} é igual a: ${divisao}`)
-        return
+    `));
+
+    if(operacao <= 0 || operacao >= 5){
+        console.log('Opção inválida!');
+        return ini();
+    }else{
+        const num01 = Number(prompt('Digite o primeiro número: '));
+        const num02 = Number(prompt('Digite o segundo número: '));
+        const {tipo, result} = calcular(num01, num02, operacao);
+        console.log(`O resultado da ${tipo} entre ${num01} e ${num02} é: ${result}`);      
     }
-
-    console.log('O número ou a operação selecionada não é válido')
-
 }
 
-calcular (numero01, numero02, operacao);
+function calcular(n1, n2, operacao){
+    switch(operacao){
+        case 1:{
+            const tipo = 'soma';
+            const result = n1+n2;
+            return {tipo, result};
+        }
+        case 2:{
+            const tipo = 'subtração';
+            const result = n1-n2;
+            return {tipo, result};
+        }
+        case 3:{
+            const tipo = 'multiplicação';
+            const result = n1*n2;
+            return {tipo, result};
+        }
+        case 4:{
+            const tipo = 'divisão';
+            const result = n1/n2;
+            return {tipo, result};
+        }
+    }
+}
